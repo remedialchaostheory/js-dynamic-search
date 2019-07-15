a0ce46b8acbf5b7cc3c0e094ab32009893fecce7
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-07-14 18:37:50 -0700

    Add search functions

--- app.js
+++ app.js
@@ -10,5 +10,27 @@
 
   console.log(cities);
 
+  function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  }
+
+  function findMatches(wordToMatch, cities) {
+    return  cities.filter(place => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      return place.city.match(regex) || place.city.match(regex);
+    });
+  }
+
+  function displayMatches() {
+    const results = findMatches(this.value, cities);
+    suggestions.innerHTML = html;
+
+  }
+
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
+
+  searchInput.addEventListener('change', displayMatches);
+  searchInput.addEventListener('keyup', displayMatches);
 
 })();
